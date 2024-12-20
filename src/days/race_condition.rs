7e73b24fb02a355603171dc045fccb4d429e3c92
Drew Hayward <drew.hayward@shopify.com>
2024-12-20 12:38:45 -0500

    day 20 pt 2

--- src/days/race_condition.rs
+++ src/days/race_condition.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     hash::Hash,
     ops::Add,
 };
@@ -184,11 +184,6 @@ impl Solution for RaceCondition {
             }
         }
 
-        //dbg!(cheat_legend
-        //    .values()
-        //    //.filter(|d| **d >= 100)
-        //    .counts_by(|d| d));
-
         cheat_legend
             .values()
             .filter(|d| **d >= 100)
@@ -197,6 +192,73 @@ impl Solution for RaceCondition {
     }
 
     fn part2(puzzle_input: String) -> String {
-        String::new()
+        let (map, start, end) = parse_input(puzzle_input);
+        let min_start_dists = find_min_dists(&map, start);
+        let min_end_dists = find_min_dists(&map, end);
+
+        let dist_to_beat = min_start_dists.get(&end).unwrap();
+
+        let mut cheat_legend: HashMap<Cheat, u64> = HashMap::new();
+
+        let spaces = map.iter().filter_map(|(p, t)| match t {
+            MapTile::Space => Some(p),
+            _ => None,
+        });
+
+        // Look for cheats
+        for space_pos in spaces {
+            // Lookup the shortest way to get to this space
+            let start_dist = match min_start_dists.get(&space_pos) {
+                Some(d) => d,
+                None => continue,
+            };
+
+            // BFS for the places we could shortcut to
+            let mut visited = HashSet::new();
+            let mut frontier = VecDeque::new();
+            frontier.push_back((*space_pos, 0));
+            //frontier.extend(DIRS.iter().filter_map(|dir| {
+            //    let neighbor = *space_pos + dir.as_point();
+            //    if let Some(MapTile::Wall) = map.get(&neighbor){
+            //        Some((neighbor, 1))
+            //    } else {
+            //        None
+            //    }
+            //}));
+            while let Some((position, dist)) = frontier.pop_front() {
+                if visited.contains(&position) {
+                    continue;
+                } else {
+                    visited.insert(position);
+                }
+
+                // Check for a cheat end
+                if let Some(end_dist) = min_end_dists.get(&position) {
+                    let cheat_dist = dist + start_dist + end_dist;
+                    if *dist_to_beat > cheat_dist {
+                        cheat_legend.insert((*space_pos, position), *dist_to_beat - cheat_dist);
+                    }
+                }
+                
+                if dist > 19 {
+                    continue;
+                }
+
+                // Neighborhood exploration
+                for neighbor_dir in DIRS {
+                    let neighbor = position + neighbor_dir.as_point();
+                    if let Some(_) = map.get(&neighbor) {
+                        frontier.push_back((neighbor, dist + 1));
+                    }
+                }
+            }
+        }
+
+
+        cheat_legend
+            .values()
+            .filter(|d| **d >= 100)
+            .count()
+            .to_string()
     }
 }
